d3d2b15f889a6ac4f408751607eb0cf02eecb38b
Omar <omarguaba54@gmail.com>
2025-05-15 13:14:33 -0400

    Header stuff

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+   <header>
+
+   <nav>
+
+    <Logo />
+
+
+    
+   </nav>
+
+
+   </header>
+  )
+}
+
+export default Header
---
+++ components/Logo.tsx
@@ -0,0 +1,14 @@
+import logo from "@/images/logos/chatlogo.jpg"
+import Image from "next/image"
+import Link from "next/link"
+
+
+
+const Logo = () => {
+  return (
+  <Link href="/">
+  </Link>
+  )
+}
+
+export default Logo
